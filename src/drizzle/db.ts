a7b693e023a2eda7e1f9003a0d196f184863c5d3
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-27 13:37:30 +0530

    db  schema import changes

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -2,6 +2,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { serverEnv } from "@/data/env/server";
+import * as schema from "./schema";
 
 const sql = neon(serverEnv.DATABASE_URL);
-export const db = drizzle(sql);
+export const db = drizzle(sql, { schema });
