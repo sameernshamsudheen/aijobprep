27b3ea2a5a4455fb6c92da72a15eeb1df647a023
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-30 02:43:37 +0530

    auth changes

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <SignIn routing="path" />
+      <SignIn routing="path" path="/sign-in" />
     </div>
   )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,13 +1,9 @@
-"use client"
 import { SignUp } from "@clerk/nextjs"
-import { usePathname } from "next/navigation"
 
 export default function SignUpPage() {
-  const path = usePathname();
-
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <SignUp routing="path" path={path ?? undefined} />
+      <SignUp routing="path" path="/sign-up" />
     </div>
   )
 }
