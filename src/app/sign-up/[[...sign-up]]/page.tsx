d00be9c846227e6b91686f6caee37232e3b44495
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-30 02:39:41 +0530

    typescript bug fix

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,13 @@
+"use client"
 import { SignUp } from "@clerk/nextjs"
+import { usePathname } from "next/navigation"
 
 export default function SignUpPage() {
+  const path = usePathname();
+
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <SignUp routing="path" />
+      <SignUp routing="path" path={path ?? undefined} />
     </div>
   )
 }
