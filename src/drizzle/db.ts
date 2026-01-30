8f33a5b0f46dfffae88ffdda4f2fcc75829b414a
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-30 15:32:45 +0530

    db chsanges

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,5 +4,10 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { serverEnv } from "@/data/env/server";
 import * as schema from "./schema";
 
-const sql = neon(serverEnv.DATABASE_URL);
+const databaseUrl = serverEnv.DATABASE_URL;
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is required");
+}
+
+const sql = neon(databaseUrl);
 export const db = drizzle(sql, { schema });
