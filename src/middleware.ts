a36012fef1e2b534a50237716efb048c8db321df
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-31 13:12:54 +0530

    bug fixes middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,32 +1,24 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
   "/",
-  "/api/webhooks(.*)",
+  "/sign-in(.*)",
+  "/sign-up(.*)",
+  "/sso-callback(.*)",
+  "/api/webhooks/clerk(.*)",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
-  const isRsc = req.headers.get("rsc") === "1";
-  const path = req.nextUrl.pathname + req.nextUrl.search;
-  const { userId, sessionId } = await auth();
-  console.log("middleware: auth", {
-    path,
-    isRsc,
-    userId,
-    sessionId,
-  });
+  // ✅ absolutely do nothing for public routes
+  if (isPublicRoute(req)) return;
 
-  if (!isPublicRoute(req)) {
-    await auth.protect();
-  }
+  // ✅ protect everything else
+  await auth.protect();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Always run for API routes
     "/(api|trpc)(.*)",
   ],
 };
