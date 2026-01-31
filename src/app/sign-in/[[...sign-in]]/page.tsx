ace7984df10381bd5e2d9023902ce5fb30836be6
sameernshamsudheen <sameernshamsudheen@gmail.com>
2026-01-31 14:18:17 +0530

    sign in change

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,8 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage() {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <SignIn/>
+      <SignIn fallbackRedirectUrl="/app"
+      signUpFallbackRedirectUrl="/onboarding"/>
     </div>
   )
 }
